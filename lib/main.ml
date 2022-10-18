92fc3ea7a7c23899926c88efb90bacccdc0c9a6b
Em Wright <ew586@cam.ac.uk>
2022-10-18 22:16:18 +0100

    Add input from files

--- lib/main.ml
+++ lib/main.ml
@@ -1,53 +1,86 @@
 (* open Ast *)
 (* open Common *)
 (* open Lexer *)
+open Printf
 open Parser
 open Evaluator
 
-
-(* Try to detect if something is getting piped in *)
-let is_interactive = 0 = (Sys.command "[ -t 0 ]")
+let handle_input db lexbuf = (
+  let dec = clause (
+      fun lb -> (
+          match Lexer.token lb with
+          | EOF -> raise Lexer.EndInput
+		      | r -> r
+		    )
+	  ) lexbuf
+  in
+  eval_dec (dec, db)
+);;
 
 let main () =
-    (if is_interactive
-     then print_endline "\nWelcome to the Prolog Interpreter\n"
-     else ()
-    );
-    let rec loop db = (
+  print_endline "\nWelcome to the Prolog Interpreter\n";
+  if Array.length Sys.argv > 2 then (
+    print_string "Too many arguments.\nExiting...\n";
+    flush stdout;
+    exit 0;
+  );
+  let initial_db =
+    if Array.length Sys.argv = 2 then (
+      printf "Opening file %s\n" Sys.argv.(1);
+      let fstream = open_in Sys.argv.(1) in
+      let rec loop db = (
         try (
-            let lexbuf = Lexing.from_channel stdin
-            in (
-                if is_interactive
-                then (print_string "> "; flush stdout)
-                else ()
-            );
-            let dec = clause (
-                fun lb -> (
-                    match Lexer.token lb with
-                    | EOF -> raise Lexer.EndInput
-		    | r -> r
-		)
-	    ) lexbuf
-            (* evaluate dec and get a new db *)
-            in let newdb = eval_dec (dec,db)
-            in loop newdb (* loop again with new db *)
-        )
-        (* exception raised *)
-        with
+          match In_channel.input_line fstream with
+          | Some s -> (
+            let lexbuf = Lexing.from_string s in
+            let newdb = handle_input db lexbuf in
+            flush stdout;
+            loop newdb
+            )
+          | None -> db
+        ) with
         | Failure s -> ( (* in case of an error *)
-            print_newline();
-	          print_endline s;
-            print_newline();
+            printf "\n Failure: %s \n" s;
+            flush stdout;
             loop db
-        )
+          )
         | Parser.Error -> ( (* failed to parse input *)
-            print_string "\nDoes not parse\n";
+            print_endline "\nDoes not parse\n";
             loop db
-        )
+          )
         | Lexer.EndInput -> exit 0 (* EOF *)
         | _ -> ( (* Any other error *)
-            print_string "\nUnrecognized error\n";
+            print_endline "\nUnrecognized error\n";
             loop db
+          )
         )
-    )
-    in (loop [])
+      in
+      let db = loop [] in
+      print_endline "\n\nFile contents loaded.\n\n";
+      db
+    ) else ( [])
+  in
+  let rec loop db = (
+    try (
+      let lexbuf = Lexing.from_channel stdin in
+      printf "> ";
+      flush stdout;
+      let newdb = handle_input db lexbuf in
+      loop newdb
+    ) with
+    | Failure s -> ( (* in case of an error *)
+        printf "\n Failure: %s \n" s;
+        flush stdout;
+        loop db
+      )
+    | Parser.Error -> ( (* failed to parse input *)
+        print_endline "\nDoes not parse\n";
+        loop db
+      )
+    | Lexer.EndInput -> exit 0 (* EOF *)
+    | _ -> ( (* Any other error *)
+        print_endline "\nUnrecognized error\n";
+        loop db
+      )
+  )
+  in loop initial_db
