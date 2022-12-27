119e063bd435b1f7ef73a94e8476257726c2c95a
Em Wright <ew586@cam.ac.uk>
2022-12-27 17:05:58 +0000

    get inheritance working

--- sequential/mutable_evaluator.ml
+++ sequential/mutable_evaluator.ml
@@ -1,113 +1,69 @@
-open Ast
+(* open Ast *)
 open Core
 open Util
-(* TODO - copy this text to a file in "Diss writing"
-   Need to write an evaluator which uses a trail stack and mutability (so probably refs).
-   Unification and stuff is already handled, so the main issue is going to be figuring out how to
-   modify and then be able to undo assignments. Probably useful to use the stack version of
-   the evaluator as the basis for this.
-   We use 3 stacks:
-   - local stack - stores env frames used to call predicates, and choice points. Stores
-   Prolog variables and return addresses. Space on this stack is allocated during execution
-   of a clause, and deallocated before the last subgoal is solved (due to tail-call optim).
-   - control stack - stores choice points (I guess instead of the local stack storing them)
-   Add choicepoints here when there are many possible clauses which apply to a goal.
-   - global stack - might not actually need for a high-level implementation
-   - trail stack - for storing assignments. Records information needed for backtracking. Is
-   therefore closely related to the control stack.
 
-   So we need to implement a loop which uses the local stack to figure out what work to do
-   We need to figure out how much to pop from the top of the trail stack when we backtrack -
-   determined by which assignments occurred at what depth of the tree - not exactly sure how
-   we keep track of this.
-*)
-
-let (fresh, reset) =
+let (fresh, current, reset) =
   let nxt = ref 0 in
-  let f () = (nxt := !nxt + 1; string_of_int (!nxt)) in
+  let f () = (nxt := !nxt + 1; !nxt) in
+  let c () = !nxt in
   let r () = nxt := 0 in
-  (f, r)
+  (f, c, r)
 
-module Var = struct
-  (* TODO kinda need a circular definition to be able to have this as a stack of Exps *)
-  type t = {subs : string Stack.t ;
-           original : string
-          }[@@deriving sexp]
+class expr () =
+  object
+    method copy = {< >}
+  end
 
-  let reset (t : t) = Stack.pop_exn t.subs |> ignore
+class exp_var () =
+  object
+    inherit expr ()
 
-  let original_value (t : t) = t.original
+    val varno = fresh ()
+    val mutable instance = None
 
-  let value t = let value = Stack.pop_exn t.subs in
-    Stack.push t.subs value; value
+    method get_varno = varno
 
-  let equal t1 t2 =
-    let v1 = value t1 in
-    let v2 = value t2 in
-    String.equal v1 v2
+    method equal (t : exp_var) =
+      Int.equal varno (t#get_varno)
 
-  let equal_original t1 t2 = String.equal (original_value t1) (original_value t2)
+    method set_instance (p : expr ref option) =
+      instance <- p
 
-  let compare t1 t2 =
-    let v1 = value t1 in
-    let v2 = value t2 in
-    String.compare v1 v2
+    method [@warning "-7"] copy =
+      let cp = {< varno = fresh (); instance = instance >} in
+      cp
 
-  let create () =
-    let og_var = fresh () in
-    let s = Stack.create () in
-    Stack.push s og_var;
-    {subs = s; original = og_var}
-end
+    method reset : unit = instance <- None
 
-module Var_table = Hashtbl.Make (String)
+  end
 
-type arithmetic_operator = PLUS | MINUS | MULT | DIV
-[@@deriving equal, sexp, compare]
+class exp_int value = object
+  inherit expr ()
 
-module Arithmetic_operand = struct
-  type t =
-      ArithmeticVar of (Var.t ref)
-    | ArithmeticInt of int
-  [@@deriving equal, sexp, compare]
+  val value : int = value
 
-  let copy t : t =
-    match t with
-    | ArithmeticVar _ -> ArithmeticVar (Var.create () |> ref)
-    | ArithmeticInt i -> ArithmeticInt i
-end
-
-
-module Exp = struct
-  type t = VarExp of (Var.t ref)             (* variables                *)
-         | IntExp of int                 (* integers                 *)
-         | TermExp of string * t list  (* functor(arg1, arg2, ...) *)
-         | ArithmeticExp of arithmetic_operator * Arithmetic_operand.t * Arithmetic_operand.t
-  [@@deriving equal, compare, sexp]
-
-  let rec copy (t : t) = match t with
-    | VarExp _ -> VarExp (Var.create () |> ref)
-    (* TODO - should this be a new var?/new variable name?
-                           If so, arithmetic variables probably should be as well *)
-    | IntExp i -> IntExp i
-    | TermExp (a, b) ->
-      let c = List.map b ~f:(fun elt -> copy elt) in
-      TermExp (a,c)
-    | ArithmeticExp (operator, op1, op2) ->
-      ArithmeticExp (operator,
-                     Arithmetic_operand.copy op1,
-                     Arithmetic_operand.copy op2
-                    )
+  method get_value : int = value
 
 end
-(* AM just has the trail as a list of variables - the base variables. Then each time an
-   assignment/unification occurs, we push this original variable name to the trail stack.
-   When we go to undo, we therefore pop a variable name from the stack, find it in the map
-   which gives the solutions, and remove the most recent assignment to that variable
-   (probably by popping from another stack). We perform assignment/unification by ...
-*)
+
+class exp_term operator_init args_init =
+  object
+    inherit expr ()
+
+    val operator : string = operator_init
+    val args : (#expr) list = args_init
+
+    method get_operator = operator
+
+    method [@warning "-7"] copy =
+      (* copy each of the args, then return another exp_term with the copies *)
+      let args2 = List.map args ~f:(fun a -> a#copy) in
+      {< operator = operator; args = args2 >}
+
+  end
+
 module Trail = struct
-  type t = (Var.t ref) Stack.t
+  type t = exp_var ref Stack.t
   type pointer = int
 
   let mark t = Stack.length t
@@ -115,28 +71,20 @@ module Trail = struct
   let undo t p =
     let to_remove = Stack.length t - p in
     for _ = 0 to to_remove do
-      let var_ref = Stack.pop_exn t in
-      Var.reset !var_ref
+      let v = Stack.pop_exn t in
+      v#reset
     done
   ;;
 
   let append t assignment = Stack.push t assignment
 end
 
-module Dec = struct
-  type t =
-    | Clause of (Exp.t ref) * (Exp.t ref list)
-    | Query of (Exp.t ref list) [@@deriving equal, sexp]
+module Var_table = Hashtbl.Make (String)
 
-  let rec copy t =
-    match t with
-    | Clause (h, t) -> let h2 = Exp.copy !h |> ref in
-      let t2 = List.map t ~f:(fun elt -> Exp.copy !elt |> ref) in
-      Clause (h2, t2)
-    | Query l -> let l2 = List.map l ~f:(fun elt -> Exp.copy !elt |> ref) in
-      Query l2
+type arithmetic_operator = PLUS | MINUS | MULT | DIV
+[@@deriving equal, sexp, compare]
 
-end
+
 
 let sub_lift_goal_arithmetic sub a =
   match a with
@@ -219,7 +167,7 @@ let rec unify trail (assignments : Exp.t Var_table.t) (s : Exp.t) (t : Exp.t) =
 let rec eval_query db _q trail =
   (* TODO - copy the db and treat it as a stack??? *)
   let rec loop db solns =
-    let new_solns = 
+    let new_solns =
       match db with
       | [] -> solns
       | r::dbs ->
